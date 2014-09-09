5527936f3d429f2ca74f4eda48754364c7e79dde
ashleynkomo <41430@longroad.ac.uk>
2014-09-09 11:05:42 +0100

    second programme (variables/text)

---
+++ Second program.py
@@ -0,0 +1,11 @@
+#Ashley Nkomo
+#09-09-14
+#2nd Program - variables & statements
+
+#this first part is input and display output
+first_name = input("Enter your first name: ")
+print(first_name)
+
+#this second part is will mix the both the variable and text
+print("Yo wassup {0}.".format(first_name))
+
