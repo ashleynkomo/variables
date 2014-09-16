feae3b6de5e0ff88b8c80c0334b5908aa34bc40c
41430 <41430@PC004140.coll.lr.local>
2014-09-16 11:01:07 +0100

    rev exercise 3

---
+++ Revision Exercise 3.py
@@ -0,0 +1,14 @@
+#Ashley Nkomo
+#16-09-14
+#Revision Exercise .3
+
+number1 = int(input("Please enter first number: "))
+number2 = int(input("Please enter second number: "))
+
+
+
+answer = number1 // number2
+remainder = number1 % number2
+
+print("Your answer is {0} and the remainder is {1}".format(answer, remainder))
+
