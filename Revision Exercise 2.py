9baca26a691bc9e7f04e49b41ab7eeddf87bd69f
41430 <41430@PC004140.coll.lr.local>
2014-09-16 10:23:18 +0100

    rev exercise 2

---
+++ Revision Exercise 2.py
@@ -0,0 +1,11 @@
+#Ashley Nkomo
+#Assignment Exercises - Revision Exercises
+#16-09-2014
+
+first_number = int(input("Please enter 1st number: "))
+second_number = int(input("Please enter 2nd number: "))
+third_number = int(input("Please enter 3rd number: "))
+
+total = first_number * second_number / third_number
+
+print("The sum of these number is {0}".format(total))
