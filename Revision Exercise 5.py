615aa5fd42d9f4f4a56a9e2d8ac4e032d6ef0b51
41430 <41430@PC004140.coll.lr.local>
2014-09-16 11:12:12 +0100

    rev exercise 5 unfinished

---
+++ Revision Exercise 5.py
@@ -0,0 +1,15 @@
+#Ashley Nkomo
+#16-09-14
+#Revision Exercise .5
+
+height_of_fridge = float(input("Please enter the height of the fridge: "))
+width_of_fridge = float(input("Please enter the width of the fridge: "))
+depth_of_fridge = float(input("Please enter the depth of the fridge: "))
+
+size_of_fridge = height_of_fridge * width_of_fridge * depth_of_fridge
+
+size_of_elevator = int(50)
+
+space_left = size_of_elevator - size_of_fridge
+
+print("Space left in elevator 
