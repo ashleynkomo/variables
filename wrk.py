6f14f1eb2b5d9a0743bf04cad223b208ecb1f46f
Ashley Nkomo <41430@longroad.ac.uk>
2014-10-16 12:15:50 +0100

    old

---
+++ wrk.py
@@ -0,0 +1,13 @@
+test_score = ("Please enter your test score: ")
+if test_score > 40:
+    print("E grade")
+elif test_score > 50:
+    print("D grade")
+elif test_score > 60:
+    print("C grade")
+elif test_score > 70:
+    print("B grade")
+elif test_score > 80:
+    print("A grade")
+else:
+    print("Fail")
